3e5643ca3e55b3f2eba810a3fa20b1bd76029d86
Anetor Eguakhide Kunle <Delwaziri@hotmail.com>
2020-02-19 02:58:44 +0100

    Third(4) major update for online cluster

--- spacenetng/static/jSfiles/afterImageLoad.js
+++ spacenetng/static/jSfiles/afterImageLoad.js
@@ -1,21 +1,36 @@
-/* eslint-disable no-unused-vars */
-import imagesloaded from 'imagesloaded';
+// /* eslint-disable no-unused-vars */
+// import imagesloaded from 'imagesloaded';
 
-imagesloaded(rectifyImages);
+// imagesloaded(rectifyImages);
 
-function rectifyImages () {
-    document.addEventListener("DOMContentLoaded", init, false); // Initialise the DOM Environment
-    function init() {
-        window.addEventListener("load", reAlignImage, false);
-    }
-    // specific function to rectify the images
-    function reAlignImage() {
-        var list1 = document.getElementsByClassName('views-left1');
-        for(let item=0; item<list1.length; item++) {
-            item.setAttribute('hidden', '');
-            item.removeAttribute('hidden');
-        }
-    }
+// function rectifyImages () {
+//     document.addEventListener("DOMContentLoaded", init, false); // Initialise the DOM Environment
+//     function init() {
+//         window.addEventListener("load", reAlignImage, false);
+//     }
+//     // specific function to rectify the images
+//     function reAlignImage() {
+//         var list1 = document.getElementsByClassName('views-left1');
+//         for(let item=0; item<list1.length; item++) {
+//             item.setAttribute('hidden', '');
+//             item.removeAttribute('hidden');
+//         }
+//     }
+// }
+
+
+
+document.addEventListener("DOMContentLoaded", init, false); // Initialise the DOM Environment
+function init() {
+    window.addEventListener("load", reAlignImage, false);
 }
 
+// specific function to rectify the images
+function reAlignImage() {
+    var list1 = document.getElementsByClassName('views-left1');
+    for(let item=0; item<list1.length; item++) {
+        item.setAttribute('hidden', '');
+        item.removeAttribute('hidden');
+    }
+}
 
